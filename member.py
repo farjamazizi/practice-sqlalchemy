ed4d09513eadb495680efd69c0ddf2d45370f7cb
farjamazizi <farjamazizi@gmail.com>
2021-05-19 11:57:44 +0430

    member

--- member.py
+++ member.py
@@ -1,36 +1,46 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+
+metadata = MetaData()
+Session = sessionmaker()
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+Session.configure(bind=engine)
+sess = Session()
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///:memory:', echo= True)
-
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
 
 class Member(Base):
-    __tablename__ = "member"
-    member_id = Column(Integer, primary_key= True)
+    __tablename__ = 'member'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String)
     last_name = Column(String)
-    id = Column(Integer, primary_key=True, autoincrement=True)
     user_name = Column(String, unique=True, nullable=False)
     password = Column(String)
 
     def __repr__(self):
-        return "<Member('%s','%s', '%s', '%s')>" % (self.first_name, self.last_name, self.user_name, self.password)
+        return "<Member('%s','%s', '%s', '%s')>" % \
+               (self.first_name, self.last_name, self.user_name, self.password)
+
+
+Base.metadata.create_all(engine)
+
 
 member1 = Member(
-    first_name = 'ali',
-    last_name = 'kalan',
-    user_name ='ali.ka',
-    password = 1370,
+    id=1,
+    first_name='ali',
+    last_name='kalan',
+    user_name='ali.ka',
+    password='1370',
 )
+sess.add(member1)
+sess.commit()
 
-session.add(member1)
-session.commit()
-
+member = sess.query(Member).filter(Member.id == 1).one_or_none()
+print(member.last_name)
 
