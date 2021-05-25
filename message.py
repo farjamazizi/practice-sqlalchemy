ad3deca4fba73f58d62268f7e6ec245a29c76ae7
farjamazizi <farjamazizi@gmail.com>
2021-05-25 10:43:08 +0430

    creat messagemodel class

---
+++ message.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, String, Integer, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+Session = sessionmaker()
+Session.configure(bind=engine)
+DBsession = Session()
+
+Base = declarative_base()
+
+
+class Message(Base):
+    __tablename__ = 'message'
+
+    id = Column(Integer, primary_key= True)
+    text = Column(Text, String, default='')
+
+    def __repr__(self):
+        return "<Message('%s')" % \
+               (self.text)
+
+
+Base.metadata.creat_all(engine)
+
+
+message1 = Message(
+    id == 1,
+    Text = 'Hello world'
+)
+
+DBsession.add(message1)
+DBsession.commit()
